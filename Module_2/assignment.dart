1fcedd0e259780506ecb422911096de39e93aa53
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-11 17:03:17 +0600

    Update assignment.dart

--- Module_2/assignment.dart
+++ Module_2/assignment.dart
@@ -1,3 +1,12 @@
+/*Task
+You are given a list of integers representing the grades obtained by a student in
+various subjects: [85, 92, 78, 65, 88, 72].
+Write a Dart program to calculate the average grade of the student.
+After calculating the average, round it to the nearest integer.
+Determine if the student's average grade is greater than or equal to 70 print Passed. 
+Print Failed otherwise
+*/
+
 void main() {
   List<int> grades = [85, 92, 78, 65, 88, 72];
   
